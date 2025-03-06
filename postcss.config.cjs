78bd8fd5bc6b2f7a423a0e1f7b032d4fb2363872
Rudyv1667 <70624271+Rudyv1667@users.noreply.github.com>
2025-03-06 19:33:50 -0300

    Fix PostCSS config

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,7 +1,7 @@
 module.exports = {
-  plugins: [
-    require('tailwindcss'),
-    require('autoprefixer'),
-    require('@tailwindcss/postcss')  // Ensure this is here
-  ],
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 };
+

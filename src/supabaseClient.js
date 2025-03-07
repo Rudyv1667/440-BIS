b2f9e76383bf1e50a808b686b489267e7cf8edb8
Rudyv1667 <70624271+Rudyv1667@users.noreply.github.com>
2025-03-07 13:53:19 -0300

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -4,6 +4,9 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseServRoleKey = import.meta.env.VITE_SUPABASE_SERVICE_ROLE_KEY;
 
+console.log("VITE_SUPABASE_URL:", import.meta.env.VITE_SUPABASE_URL);
+console.log("VITE_SUPABASE_ANON_KEY:", import.meta.env.VITE_SUPABASE_ANON_KEY);
+
 if (!supabaseUrl || !supabaseServRoleKey) {
   throw new Error("Missing Supabase environment variables!");
 }
